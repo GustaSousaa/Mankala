040331df705179b87e638f7417bec95be844f345
enzomaneira <notebookparavender@MacBook-de-Para.local>
2023-11-06 15:06:06 -0300

    criando repo

---
+++ src/Player.java
@@ -0,0 +1,32 @@
+public class Player
+{
+    String nome;
+    boolean vez;
+    int repositorio;
+    
+    void criarJogador(String nome, int repositorio){
+        this.nome = nome;
+        this.vez = false;
+        this.repositorio = repositorio;
+    }
+    
+    void setVez(){
+        if(vez==true){
+            vez = false;
+        }else{
+            vez = true;
+        }
+    }
+    
+    boolean getVez(){
+        return vez;
+    }
+    
+    String getNome(){
+        return nome;
+    }
+    
+    int getRepositorio(){
+        return repositorio;
+    }
+}
